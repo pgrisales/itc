55d3f790ab8fa343c9f2d4873d4f29c1e6853d80
JuanDiego98 <jlozanoc@unal.edu.co>
2023-06-21 12:59:30 -0500

    Fixed minimization

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 from test import *
 
-
 if __name__ == '__main__':
   #test_AFPD()
   #test_AFPN()
-  test_AF2P()
-  #probarSimplificacion()
-
+  #test_AF2P()
+  test_Simplificacion()
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from processing import read_file
+from automatas import *
 
 def test_AFD():
   input_file = './inputs/input.txt'
@@ -47,32 +48,34 @@ def test_Simplificacion():
   f1 = './inputs/simplificar1.txt'
   f2 = './inputs/simplificar2.txt'
   f3 = './inputs/simplificar3.txt'
-
-  print('')
-  print('Autómata M1:')
-  print('')
+  
+  print('\nSimplificación AFD:\n\nAutómata M1:\n')
   a = read_file(f1)
-  print('')
-  print('Autómata M2:')
-  print('')
-  b = read_file(f2)
-  print('')
-  print('Autómata M3:')
-  print('')
-  c = read_file(f3)
+  print('Estado inicial: ' + str(a.init_state))
+  print('Estados de aceptación: ' + str(a.accepting_states) + '\n')
 
-  print('')
-  print('Autómata M1\':')
-  print('')
-  AFD.simplificarAFD(a)
-  print('')
-  print('Autómata M2\':')
-  print('')
-  AFD.simplificarAFD(b)
-  print('')
-  print('Autómata M3\':')
-  print('')
-  AFD.simplificarAFD(c)
+  print('\nAutómata M2:\n')
+  b = read_file(f2)
+  print('Estado inicial: ' + str(b.init_state))
+  print('Estados de aceptación: ' + str(b.accepting_states) + '\n')
+
+  print('\nAutómata M3:\n')
+  c = read_file(f3)
+  print('Estado inicial: ' + str(c.init_state))
+  print('Estados de aceptación: ' + str(c.accepting_states) + '\n')
+
+  print('\nAutómata M1\':\n')
+  A = AFD.simplificarAFD(a)
+  print('Estado inicial: ' + str(A.init_state))
+  print('Estados de aceptación: ' + str(A.accepting_states) + '\n')
+  print('\nAutómata M2\':\n')
+  B = AFD.simplificarAFD(b)
+  print('Estado inicial: ' + str(B.init_state))
+  print('Estados de aceptación: ' + str(B.accepting_states) + '\n')
+  print('\nAutómata M3\':\n')
+  C = AFD.simplificarAFD(c)
+  print('Estado inicial: ' + str(C.init_state))
+  print('Estados de aceptación: ' + str(C.accepting_states) + '\n')
 
 def test_producto_cartesiano():
     f1 = './inputs/cartesiano1.txt'
